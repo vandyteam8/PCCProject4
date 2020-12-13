a8fe0104029e12e3cbd07f738ab76b807b2bfc32
Mahesh Gouru <mgouru@my.loyno.edu>
2020-12-12 19:23:32 -0500

    spacing

--- KafkaStreamsPython/consumer.py
+++ KafkaStreamsPython/consumer.py
@@ -34,8 +34,7 @@ dbname = "project4/"
 def externalConsumer(ipaddr):
     # acquire the consumer
     # (you will need to change this to your bootstrap server's IP addr)
-    consumer = KafkaConsumer(bootstrap_servers="{}:9092".format(LOCALHOST),
-                    value_deserializer = lambda m: json.loads(m.decode('utf-8')))
+    consumer = KafkaConsumer(bootstrap_servers="{}:9092".format(LOCALHOST), value_deserializer = lambda m: json.loads(m.decode('utf-8')))
     consumer.subscribe(topics=["energyutilization"])
 
     # we keep reading and printing
